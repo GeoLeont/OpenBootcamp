629408330963d62eff0da0ae0ffb235dea74feb0
GeoLeont <galeon18@misena.edu.co>
2022-08-15 00:11:31 -0500

    Agrego correcciones al ejercicio 9 del tema 9

--- IntroduccionProgramacion/EjercicioTema9/src/Main.java
+++ IntroduccionProgramacion/EjercicioTema9/src/Main.java
@@ -3,10 +3,18 @@ import java.util.concurrent.Callable;
 public class Main {
     public static void main(String[] args) {
         Cliente cliente = new Cliente();
+        Trabajador trabajador = new Trabajador();
         System.out.println("Nombre: "+ cliente.nombre);
         System.out.println("Edad: "+ cliente.edad);
         System.out.println("Telefono: " + cliente.telefono);
         System.out.println("Credito: " + cliente.credito);
+
+        trabajador.edad=27;
+        trabajador.nombre= "Jose";
+        trabajador.salario = 5200;
+        trabajador.telefono = 859632147;
+
+        System.out.println("Hola soy "+ trabajador.nombre+" tengo "+ trabajador.edad +" anos y mi salario es de "+ trabajador.salario+" Euros. Y este es mi numero telefonico: "+trabajador.telefono);
 
 
 
